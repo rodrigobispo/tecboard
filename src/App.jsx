04c7404cc0779b577fbab667aa4347b5f4ce751b
Rodrigo Augusto Carlos Bispo <rodrigo.bispo@infrasa.gov.br>
2025-12-05 15:28:58 -0300

    feat: passagens de parametrosa componentes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,52 @@
 import './App.css'
 
+// no react, componentes são FUNÇÕES
+
+function TituloFormulario(props) {
+  return (
+    <h2>{props.children}</h2>
+  )
+}
+
+function CampoDeFormulario({ children, htmlFor }) {
+  return (
+    <fieldset htmlFor={htmlFor}>
+      {children}
+    </fieldset>
+  )
+}
+
+function Label({ children }) {
+  return (
+    <label>
+      {children}
+    </label>
+  )
+}
+
+function CampoDeEntrada(props) {
+  return <input {...props} />
+}
+
+function FormularioDeEvento() {
+    return (
+        <form className='form-evento'>
+          <TituloFormulario>
+            Preencha para criar um evento:
+          </TituloFormulario>
+          <CampoDeFormulario>
+            <Label htmlFor="nome">Qual é o nome do evento?</Label>
+            <CampoDeEntrada
+              type="text"
+              id='nome'
+              placeholder='Summer dev hits'
+              name='nomeEvento'
+            />
+          </CampoDeFormulario>
+        </form>
+    )
+}
+
 function App() {
   return (
     <main>
@@ -9,6 +56,7 @@ function App() {
         <section>
             <img src="/banner.png" alt="" />
         </section>
+        <FormularioDeEvento />
     </main>
   )
 }
