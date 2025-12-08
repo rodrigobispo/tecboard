1b9dfed9ed88ffe8fc82b1fbd67424c021e0cc65
Rodrigo Augusto Carlos Bispo <rodrigo.bispo@infrasa.gov.br>
2025-12-07 22:22:55 -0300

    feat: filtros de temas e eventos.

--- src/App.jsx
+++ src/App.jsx
@@ -58,17 +58,29 @@ function App() {
         temas={temas}
         aoSubmeter={adicionarEvento}
       />
+      <section className='container'>
+        {temas.map(function (tema) {
+          const temEvento = eventos.some(
+            evento => evento.tema.id === tema.id
+          )
+          if (!temEvento){
+            return null
+          }
+          return (
+            <section key={tema.id}>
+              <Tema tema={tema} />
+              <div className="eventos">
+                {eventos
+                  .filter(evento => evento.tema.id === tema.id)
+                  .map(function (evento, index) {
+                  return <CardEvento evento={evento} key={index} />
+                })}
+              </div>
+            </section>
+          )
+        })}
+      </section>
 
-      {temas.map(function (item) {
-        return (
-          <section key={item.id}>
-            <Tema tema={item} />
-            {eventos.map(function (item, index) {
-              return <CardEvento evento={item} key={index} />
-            })}
-          </section>
-        )
-      })}
     </main>
   )
 }
